75b7531df04d01a81990bf8fd4ef015e7005236e
Chtonyss <68635442+Chtonyss@users.noreply.github.com>
2026-08-29 10:29:04 +0900

    Update NativeProxy.kt

--- app/src/main/java/com/amurcanov/tgwsproxy/NativeProxy.kt
+++ app/src/main/java/com/amurcanov/tgwsproxy/NativeProxy.kt
@@ -14,6 +14,7 @@ interface ProxyLibrary : Library {
     fun SetPoolSize(size: Int)
     fun SetCfProxyCacheDir(cacheDir: String)
     fun SetCfProxyConfig(enabled: Int, priority: Int, userDomain: String)
+	fun UpdateDcConfig(dcIps: String): Int
     fun GetSecretWithPrefix(): Pointer?
     fun GetStats(): Pointer?
     fun FreeString(p: Pointer)
@@ -57,5 +58,8 @@ object NativeProxy {
         val res = ptr.getString(0)
         ProxyLibrary.INSTANCE.FreeString(ptr)
         return res
+    }
+	fun updateDcConfig(dcIps: String): Int {
+        return ProxyLibrary.INSTANCE.UpdateDcConfig(dcIps)
     }
 }
